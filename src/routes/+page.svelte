e18a80ead38fdb22d4fd3a642d1d7b6825390b21
Jamey Smith <smith.jameyd@gmail.com>
2023-12-12 11:04:55 +0100

    feat: add mouse tracking and card rotation tween

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,22 +4,36 @@
   // Props
 
   // Constant Variables
+  const centerX = window.innerWidth / 2
+  const centerY = window.innerHeight / 2
 
   // State Variables
+  let m = { x: 0, y: 0 };
   let card: HTMLElement;
 
   // Event Handlers
+  const handleMove = (event: PointerEvent) => {
+    m.x = event.clientX
+    m.y = event.clientY
 
+
+    // Get offset of pointer from center
+    const offsetX = ((m.x - centerX) / centerX) * 30
+    const offsetY = ((m.y - centerY) / centerY) * 30
+
+    gsap.to(card, {rotateX: -1 * offsetY, rotateY: offsetX})
+  }
   // Lifecycle Functions
 
   // Reactive Statements
+
 </script>
 
 
 
-<main class="h-full w-full grid place-items-center">
+<main on:pointermove={handleMove} class="h-full w-full grid place-items-center">
   <article bind:this={card} class="h-96 w-80 bg-gradient-to-tr from-cyan-500 to-fuchsia-500 rounded-2xl shadow-lg relative">
-    <div class="bg-gray-800 absolute inset-3 rounded-2xl blur-lg"></div>
+    <div id="shadow" class="bg-gray-900 absolute inset-3 rounded-2xl blur-xl"></div>
     <div class="bg-gray-900 absolute inset-4 rounded-2xl"></div>
   </article>
 </main>
@@ -32,6 +46,10 @@
 
   div {
     transform-style: preserve-3d;
-    transform: translateZ(100px);
+    transform: translateZ(50px);
+  }
+
+  #shadow {
+    transform: translateZ(1px)
   }
 </style>
